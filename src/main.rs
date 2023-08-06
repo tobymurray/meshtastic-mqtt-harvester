f5fb74cbcd9ee2d8e349390578556141c4a367c6
Toby Murray <toby.murray@protonmail.com>
2023-08-06 16:52:47 -0400

    Extract some configuration with once_cell

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,6 @@
 use crate::mqtt::handle_incoming;
 use dotenvy::dotenv;
+use once_cell::sync::Lazy;
 use rumqttc::{AsyncClient, MqttOptions, QoS};
 use std::time::Duration;
 
@@ -8,20 +9,25 @@ pub mod postgres;
 pub mod utils;
 
 pub mod meshtastic {
+	// Something like target/debug/build/meshtastic-mqtt-harvester-<hash>/out/meshtastic.rs
 	include!(concat!(env!("OUT_DIR"), "/meshtastic.rs"));
 }
 
-#[tokio::main]
-async fn main() {
-	dotenv().ok();
+static MQTT_CONFIG: Lazy<MqttOptions> = Lazy::new(|| {
 	let mqtt_host = std::env::var("MQTT_HOST").unwrap();
 	let mqtt_port = std::env::var("MQTT_PORT").unwrap().parse::<u16>().unwrap();
 	let mqtt_user = std::env::var("MQTT_USER").unwrap();
 
 	let mut mqttoptions = MqttOptions::new(mqtt_user, mqtt_host, mqtt_port);
 	mqttoptions.set_keep_alive(Duration::from_secs(5));
+	mqttoptions
+});
 
-	let (client, mut eventloop) = AsyncClient::new(mqttoptions, 10);
+#[tokio::main]
+async fn main() {
+	dotenv().ok();
+
+	let (client, mut eventloop) = AsyncClient::new(MQTT_CONFIG.clone(), 10);
 	client.subscribe("msh/2/c/LongFast/#", QoS::AtMostOnce).await.unwrap();
 
 	loop {
--- src/postgres.rs
+++ src/postgres.rs
@@ -1,16 +1,27 @@
 use chrono::{DateTime, Utc};
+use once_cell::sync::Lazy;
 use tokio_postgres::NoTls;
 
-pub async fn setup() -> tokio_postgres::Client {
+static CONFIG: Lazy<String> = Lazy::new(|| {
 	let postgres_database = std::env::var("POSTGRES_DATABASE").unwrap();
 	let postgres_host = std::env::var("POSTGRES_HOST").unwrap();
 	let postgres_password = std::env::var("POSTGRES_PASSWORD").unwrap();
 	let postgres_port = std::env::var("POSTGRES_PORT").unwrap();
 	let postgres_user = std::env::var("POSTGRES_USER").unwrap();
 
-	let config = format!("host={postgres_host} user={postgres_user} port={postgres_port} dbname={postgres_database} password={postgres_password}");
+	format!("host={postgres_host} user={postgres_user} port={postgres_port} dbname={postgres_database} password={postgres_password}")
+});
 
-	let (client, connection) = tokio_postgres::connect(&config, NoTls).await.unwrap();
+static INSERT_STATEMENT: Lazy<String> = Lazy::new(|| {
+	let table = std::env::var("POSTGRES_TABLE").unwrap();
+
+	format!(
+		"INSERT INTO {table} (user_id, location, timestamp) VALUES ($1, ST_SetSRID(ST_MakePoint($2, $3), 4326), $4)"
+	)
+});
+
+pub async fn setup() -> tokio_postgres::Client {
+	let (client, connection) = tokio_postgres::connect(&CONFIG, NoTls).await.unwrap();
 
 	tokio::spawn(async move {
 		if let Err(e) = connection.await {
@@ -28,16 +39,8 @@ pub async fn insert_location(
 	longitude: f64,
 	timestamp: DateTime<Utc>,
 ) -> u64 {
-	let table = std::env::var("POSTGRES_TABLE").unwrap();
+	let stmt = client.prepare(&INSERT_STATEMENT).await.unwrap();
 
-	let stmt = format!(
-		"INSERT INTO {table} (user_id, location, timestamp) VALUES ($1, ST_SetSRID(ST_MakePoint($2, $3), 4326), $4)"
-	);
-
-	// Define the SQL statement with placeholders for the parameters
-	let stmt = client.prepare(&stmt).await.unwrap();
-
-	// Execute the prepared statement with the provided arguments
 	client
 		.execute(&stmt, &[&user_id, &longitude, &latitude, &timestamp])
 		.await
